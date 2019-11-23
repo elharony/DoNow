b72e6c2175703bb28b63f74b5c7d503c1071d7c4
elharony <yahya@elharony.com>
2019-11-23 05:01:32 +0200

    test: Highlight the filter according to the current view/route

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -176,11 +176,16 @@ describe('controller', function () {
 
 	it('should highlight "All" filter by default', function () {
 		// TODO: write test
+		subject.setView('');
 
+		expect(view.render).toHaveBeenCalledWith("setFilter", "");
 	});
 
 	it('should highlight "Active" filter when switching to active view', function () {
 		// TODO: write test
+		subject.setView('#/active');
+
+		expect(view.render).toHaveBeenCalledWith("setFilter", "active");
 	});
 
 	describe('toggle all', function () {
